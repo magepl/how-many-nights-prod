fbd1e611d40a9107396eaaa46699ca069aef4dc1
Matt <95616571+magepl@users.noreply.github.com>
2025-02-24 18:45:17 +0000

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+function daysBetweenDates(date1: string, date2: string): number {
+  const startDate = new Date(date1);
+  const endDate = new Date(date2);
+  // Calculate the difference in milliseconds
+  const differenceInTime = endDate.getTime() - startDate.getTime();
+  console.log(differenceInTime);
+  // Convert milliseconds to days
+  const differenceInDays = differenceInTime / (1000 * 3600 * 24);
+  console.log(differenceInDays);
+  return Math.abs(Math.round(differenceInDays)); // Return absolute value of days
+  console.log(differenceInDays);
+}
+
+document.getElementById("dateForm")?.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log("click");
+  const startDate = (
+    document.getElementById("start-date-el") as HTMLInputElement
+  ).value;
+  const endDate = (document.getElementById("end-date-el") as HTMLInputElement)
+    .value;
+
+  const daysDifference = daysBetweenDates(endDate, startDate);
+  document.getElementById("total-el")!.innerText = `Total: ${daysDifference}`;
+});
